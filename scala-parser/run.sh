2639841d3278e16072ccc893f275346e1628ac44
suzuki-navi <suzuki.navi.dev@gmail.com>
2019-07-05 19:41:33 +0900

    use anylang in run.sh

--- scala-parser/run.sh
+++ scala-parser/run.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p var/class_file
 make -s var/class_file/Main.class
-scala -cp var/class_file Main "$@"
+bash ./var/anylang.sh --jdk=11 --scala=2.13.0 scala -cp var/class_file Main "$@"
