b27f15546037a6ecd2bae448792358fa97106e32
suzuki-navi <suzuki.navi.dev@gmail.com>
2019-08-03 22:24:38 +0900

    parser.scala

--- scala-parser/src/parser.scala
+++ scala-parser/src/parser.scala
@@ -4,7 +4,42 @@ object OptionParser {
 
   def parse(args: List[String], ctxt: OptionParserContext):
     Either[ParserMessage, OptionParserStatus] = {
-    throw new AssertionError("TODO");
+
+    @scala.annotation.tailrec
+    def sub(status: OptionParserStatus, args: List[String], argIdx: Int):
+      Either[ParserMessage, OptionParserStatus] = {
+      args match {
+        case Nil =>
+          Right(status);
+        case arg :: tail =>
+          status.eat(arg, tail, argIdx, ctxt) match {
+            case (Left(msg: ParserMessage), _) =>
+              Left(msg);
+            case (Right(status), Some((args, argIdx))) =>
+              sub(status, args, argIdx);
+            case (Right(status), None) =>
+              Right(status);
+          }
+      }
+    }
+
+    val status = OptionParserStatus.init(CommandSeqInputType.SomeInput, CommandSeqOutputType.SomeOutput);
+    sub(status, args, 0);
+  }
+
+  def isOption(arg: String): Boolean = {
+    arg.startsWith("-") || arg == "[";
+  }
+
+  val commands: Map[String, CommandParser] = Map (
+    //"cut" -> CutCommandParser,
+    //"diff" -> DiffCommandParser,
+  );
+
+  def commandExists(name: String): Boolean = commands.contains(name);
+
+  def command(name: String, argIdx: Int): CommandParserStatus = {
+    commands(name).initStatus(argIdx);
   }
 
 }
@@ -32,12 +67,85 @@ case class OptionParserStatus (
   outputFile: Option[String],
   inputFileArgIdx: Int,
   outputFileArgIdx: Int,
-  //lastCommand: CommandParserStatus,
+  commands: Vector[CommandNode],
+  lastCommand: CommandParserStatus,
 ) {
 
-  def eat(args: List[String], argIdx: Int, ctxt: OptionParserContext):
+  def eat(arg: String, tail: List[String], argIdx: Int, ctxt: OptionParserContext):
     (Either[ParserMessage, OptionParserStatus], Option[(List[String], Int)]) = {
-    throw new AssertionError("TODO");
+    if (OptionParser.isOption(arg)) {
+      if (arg == "--help") {
+        // --help はすべてのコマンドで共通のオプションであるため最初にチェック
+        (Left(lastCommand.help), None);
+      } else {
+        // コマンドのオプションとして解釈を試みる
+        lastCommand.eatOption(arg, tail, argIdx, ctxt) match {
+          case None =>
+            // コマンドのオプションとして解釈できなかったケース
+            // グローバルのオプションとして解釈を試みる
+            eatOption(arg, tail, argIdx, ctxt);
+          case Some((Right(cmd), opt)) =>
+            // コマンドのオプションとして解釈できたケース
+            (Right(this.copy(lastCommand = cmd)), opt);
+          case Some((Left(err), opt)) =>
+            // コマンドのオプションとして解釈できたけどエラーのケース
+            (Left(err), opt);
+        }
+      }
+    } else { // - 以外で始まる引数
+      if (OptionParser.commandExists(arg)) {
+        // コマンド名と同じ名前のファイルが偶然存在していたとしてもコマンド名を優先する。
+        // 実行環境による解釈の揺れを少しでも減らすため。
+        val nextCommand = OptionParser.command(arg, argIdx);
+        if (lastCommand == NoneCommandParserStatus) {
+          (Right(this.copy(lastCommand = nextCommand)), Some((tail, argIdx + 1)));
+        } else {
+          lastCommand.finish match {
+            case Left(err) =>
+              (Left(err), None);
+            case Right(cmd) =>
+              (Right(this.copy(commands = commands :+ cmd, lastCommand = nextCommand)), Some((tail, argIdx + 1)));
+          }
+        }
+      } else if (ctxt.inputFileExists(arg) && inputFile.isEmpty) {
+        (Right(this.copy(inputFile = Some(arg), inputFileArgIdx = argIdx)), Some((tail, argIdx + 1)));
+      } else if (ctxt.inputFileExists(arg) && lastCommand == NoneCommandParserStatus) {
+        (Left(ParserErrorMessage(argIdx, "duplicated parameter")), None);
+      } else if (!ctxt.inputFileExists(arg) && lastCommand == NoneCommandParserStatus) {
+        (Left(ParserErrorMessage(argIdx, "file not found")), None);
+      } else {
+        lastCommand.eatArgument(arg, tail, argIdx, ctxt) match {
+          case Some((Right(cmd), opt)) =>
+            (Right(this.copy(lastCommand = cmd)), opt);
+          case Some((Left(err), opt)) =>
+            (Left(err), opt);
+          case None =>
+            (Left(ParserErrorMessage(argIdx, "unknown argument")), None);
+        }
+      }
+    }
+  }
+
+  private[this] def eatOption(opt: String, tail: List[String], argIdx: Int, ctxt: OptionParserContext):
+    (Either[ParserMessage, OptionParserStatus], Option[(List[String], Int)]) = {
+    if (opt == "--tsv") {
+      inputFormat match {
+        case Some(_) =>
+          (Left(ParserErrorMessage(argIdx, "duplicated option")), None);
+        case None =>
+          (Right(this.copy(inputFormat = Some(TsvInputFormat))), Some((tail, argIdx + 1)));
+      }
+    } else if (opt == "--csv") {
+      throw new AssertionError("TODO");
+    } else if (opt == "-") {
+      throw new AssertionError("TODO");
+    } else if (opt == "-i") {
+      throw new AssertionError("TODO");
+    } else if (opt == "-o") {
+      throw new AssertionError("TODO");
+    } else {
+      (Left(ParserErrorMessage(argIdx, "unknown option")), None);
+    }
   }
 
   def completion(arg: String, argIdx: Int, ctxt: OptionParserContext):
@@ -45,12 +153,26 @@ case class OptionParserStatus (
     throw new AssertionError("TODO");
   }
 
+  def finish: Either[ParserErrorMessage, CommandNodeSeq] = {
+    throw new AssertionError("TODO");
+  }
+
 }
 
 object OptionParserStatus {
-  def init(inputType: CommandSeqInputType, outputType: CommandSeqOutputType): OptionParserStatus = {
-    throw new AssertionError("TODO");
-  }
+
+  def init(inputType: CommandSeqInputType, outputType: CommandSeqOutputType) = OptionParserStatus(
+    inputType = inputType,
+    outputType = outputType,
+    inputFormat = None,
+    inputFile = None,
+    outputFile = None,
+    inputFileArgIdx = 0,
+    outputFileArgIdx = 0,
+    commands = Vector.empty,
+    lastCommand = NoneCommandParserStatus,
+  );
+
 }
 
 sealed trait ParserMessage;
@@ -62,6 +184,18 @@ trait OptionCompletion {
   def parameters: List[String];
   def options: List[String];
   def commandsEnable: Boolean;
+}
+
+case class CommandNodeSeq (
+  inputType: CommandSeqInputType,
+  outputType: CommandSeqOutputType,
+  inputFormat: Option[InputFormat],
+  inputFile: Option[String], // 空文字列は標準入力の意味
+  outputFile: Option[String],
+  inputFileArgIdx: Int,
+  outputFileArgIdx: Int,
+  commands: Vector[CommandNode],
+) {
 }
 
 sealed trait InputFormat { def name: String }
@@ -87,14 +221,94 @@ case object CsvInputFormat extends InputFormat { def name = "csv" }
 //         outputType = SomeOutputOrNone
 sealed trait CommandSeqInputType;
 object CommandSeqInputType {
-  case object SomeInput;
-  case object SomeInputOrNone;
-  case object NoneInput;
+  case object SomeInput extends CommandSeqInputType;
+  case object SomeInputOrNone extends CommandSeqInputType;
+  case object NoneInput extends CommandSeqInputType;
 }
 sealed trait CommandSeqOutputType;
 object CommandSeqOutputType {
-  case object SomeOutput;
-  case object SomeOutputOrNone;
-  case object NoneOutput;
+  case object SomeOutput extends CommandSeqOutputType;
+  case object SomeOutputOrNone extends CommandSeqOutputType;
+  case object NoneOutput extends CommandSeqOutputType;
+}
+
+sealed trait ChildCommandSeqType;
+object ChildCommandSeqType {
+  case object NormalChildCommandSeqType extends ChildCommandSeqType;
+  case object TailChildCommandSeqType extends ChildCommandSeqType;
+}
+
+trait CommandParser {
+  def initStatus(argIdx: Int): CommandParserStatus;
+}
+
+trait CommandParserStatus {
+
+  def childParser: Option[(OptionParserStatus, ChildCommandSeqType)];
+
+  def updateChildParser(childParser: OptionParserStatus): CommandParserStatus;
+
+  def endChildParser(childCommands: CommandNodeSeq): CommandParserStatus;
+
+  def help: HelpDocument;
+
+  def eatOption(opt: String, tail: List[String], argIdx: Int, ctxt: OptionParserContext):
+    Option[(Either[ParserErrorMessage, CommandParserStatus], Option[(List[String], Int)])];
+
+  def eatArgument(arg: String, tail: List[String], argIdx: Int, ctxt: OptionParserContext):
+    Option[(Either[ParserErrorMessage, CommandParserStatus], Option[(List[String], Int)])];
+
+  def finish: Either[ParserErrorMessage, CommandNode];
+
+}
+
+case object NoneCommandParserStatus extends CommandParserStatus {
+
+  def childParser: Option[(OptionParserStatus, ChildCommandSeqType)] = {
+    None;
+  }
+
+  def updateChildParser(childParser: OptionParserStatus): CommandParserStatus = {
+    throw new AssertionError();
+  }
+
+  def endChildParser(childCommands: CommandNodeSeq): CommandParserStatus = {
+    throw new AssertionError();
+  }
+
+  def help: HelpDocument = {
+    throw new AssertionError("TODO");
+  }
+
+  def eatOption(opt: String, tail: List[String], argIdx: Int, ctxt: OptionParserContext):
+    Option[(Either[ParserErrorMessage, CommandParserStatus], Option[(List[String], Int)])] = {
+    None;
+  }
+
+  def eatArgument(arg: String, tail: List[String], argIdx: Int, ctxt: OptionParserContext):
+    Option[(Either[ParserErrorMessage, CommandParserStatus], Option[(List[String], Int)])] = {
+    None;
+  }
+
+  def finish: Either[ParserErrorMessage, CommandNode] = throw new AssertionError();
+
+}
+
+trait CommandNode {
+
+  // 出力がTSV形式かどうか
+  // TSV形式でない場合はこの後ろに次のコマンドを配置することができない
+  def isOutputTsv: Boolean;
+
+  //// CommandGraphにてノードとして扱うかどうか
+  //def isCommandGraphNode: Boolean;
+
+  //// CommandGraphにてノードとして扱う場合に前後のコマンド列を含めてエッジとして追加する
+  //def addNodeToGraph(graph: CommandGraph,
+  //  prevCommands: Vector[CommandSeqNode], nextCommands: Vector[CommandSeqNode],
+  //  inputEdgeId: Int, outputEdgeId: Int): CommandGraph;
+
+  //def toTreeString: List[String];
+
 }
 
